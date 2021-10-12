2091f7eb47c36745b63c4e880e4deaa3814ee439
Kelly Solano <kelly.solano.mt@correo.usa.edu.co>
2021-10-11 23:58:29 -0500

    Segundo commit

--- src/main/java/com/usa/ciclo3/ciclo3/repository/crud/SkateboardCrudRepository.java
+++ src/main/java/com/usa/ciclo3/ciclo3/repository/crud/SkateboardCrudRepository.java
@@ -1,8 +1,9 @@
 package com.usa.ciclo3.ciclo3.repository.crud;
 
 import com.usa.ciclo3.ciclo3.model.Skateboard;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SkateboardCrudRepository extends CrudRepository<Skateboard, Integer> {
+public interface SkateboardCrudRepository extends JpaRepository<Skateboard, Integer> {
 
 }
